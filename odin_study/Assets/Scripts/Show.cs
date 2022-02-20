2ea3ea419eb5c6c91ce7fc44871f722f44204afc
CodingCodingK <2470939431@qq.com>
2022-02-20 18:52:15 +0800

    0220

---
+++ odin_study/Assets/Scripts/Show.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Show : MonoBehaviour
+{
+    public Sprite m_OriginSprite;
+
+    public List<int> m_OriginList = new List<int>();
+
+    public Dictionary<int, int> m_OriginDic = new Dictionary<int, int>();
+}
